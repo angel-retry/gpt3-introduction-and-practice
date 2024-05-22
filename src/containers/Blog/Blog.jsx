8ef75024399b6e185e15e89fbfc99bb9618df46c
unknown <a27657060@gmail.com>
2024-05-22 09:38:59 +0800

    feat: add styled in Blog conponent

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -6,10 +6,10 @@ const Blog = () => {
   return (
     <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
-        <div className="gradient__text">
+        <h1 className="gradient__text">
           A lot is happening,
           We are blogging about it.
-        </div>
+        </h1>
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
